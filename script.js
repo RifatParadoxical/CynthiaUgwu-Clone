eea8e62cdc74ff7215c4f49ec775b977b849e32e
RIFAT PARADOXICAL <137922327+RifatParadoxical@users.noreply.github.com>
2025-01-03 19:43:17 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('.main'),
+  smooth: true
+});
+
+function mouseChapta() {
+  var xscale = 1;
+  var yscale = 1;
+
+  var xprev = 0 ;
+  var yprev = 0 ;
+  document.addEventListener("mousemove", function(event){
+    clearTimeout()
+    var xdiff = event.clientX - xprev;
+    xprev = event.clientX;
+
+    var ydiff = event.clientY - yprev;
+    yprev = event.clientY;
+    var xchapta = gsap.utils.clamp(0.8, 1.2, xdiff)
+    var ychapta = gsap.utils.clamp(0.8, 1.2, ydiff)
+    circleCursor(xchapta, ychapta)
+    timeout = setTimeout(() => {
+      document.querySelector(".cursor").style.transform = `translate(${event.clientX}px , ${event.clientY}px) scale(1,1)`
+    }, 50);
+  })
+}
+mouseChapta()
+
+function circleCursor( xchapta, ychapta){
+  window.addEventListener("mousemove", function(event){
+    document.querySelector(".cursor").style.transform = `translate(${event.clientX}px , ${event.clientY}px) scale(${xchapta}, ${ychapta})`
+  })
+}
+circleCursor();
+function firstPageAnim(){
+  var tl = gsap.timeline();
+  tl.from("nav", {
+    y: "-10",
+    opacity: 0,
+    duration: 1.5,
+    ease: Expo.easeInOut
+  })
+  .to(".boundingelem",{
+    y:0,
+    ease: Expo.easeInOut,
+    duration: 2,
+    delay: -1,
+    stagger: .2
+  })
+  .from(".footer", {
+    y: -10,
+    opacity: 0,
+    duration:1.5,
+    ease: Expo.easeInOut,
+    delay: -1
+  })
+}
+firstPageAnim()
+
+document.querySelectorAll(".elem").forEach(function(elem) {
+  elem.addEventListener("mousemove", function(event) {
+      var imgHeight = elem.querySelector('img').offsetHeight;
+      var imgWidth = elem.querySelector('img').offsetWidth;
+
+      var topPosition = event.clientY - elem.getBoundingClientRect().top - (imgHeight / 2);
+      var leftPosition = event.clientX - (imgWidth / 2); 
+
+      gsap.to(elem.querySelector('img'), {
+          opacity: 1,
+          ease: Power1,
+          top: topPosition,
+          left: leftPosition,
+      });
+  });
+
+  elem.addEventListener("mouseleave", function(event) {
+      gsap.to(elem.querySelector('img'), {
+          opacity: 0,
+          ease: Power1,
+      });
+  });
+});
